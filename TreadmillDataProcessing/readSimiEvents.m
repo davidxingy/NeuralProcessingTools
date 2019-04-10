3657716e36734a33696da030ae10a3e3eb4239fb
David Xing <davidxingg@gmail.com>
2019-04-10 18:17:29 -0400

    Added proper function help block

--- TreadmillDataProcessing/readSimiEvents.m
+++ TreadmillDataProcessing/readSimiEvents.m
@@ -1,12 +1,34 @@
-function Events=readSimiEvents(fullFileName,trialNum,markerNames,varargin)
+function Events = readSimiEvents(fullFileName, blockNum, markerNames, varargin)
+% Events = readSimiEvents(fullFileName, blockNum, markerNames, [Events])
 % 
 % Function to read in Simi .p files which contain the marked events and
 % extract the frames where the events are marked. Assumes that the .p files
-% follow the structure of header lines followed by a blank line, followed
+% follow the structure of: header lines followed by a blank line, followed
 % by the marker IDs, followed by the marker names, then the data as
 % delimited values. If you want to append the extracted events to an
 % existing events struct, you can give that events struct as an input.
 % 
+% Inputs:
+% fullFileName -    String containing the path and filename of the simi .p
+%                   file containing the marked events
+% 
+% blockNum -        Which block to add the new extracted events to (since
+%                   the Events struct is arranged by blocks)
+% 
+% markerNames -     The names in the labels in the .p file whos events we
+%                   want to extract. Has to be an exact match (case
+%                   sensitive)
+% 
+% Events -          Optional. Nx1 struct, with fields corresponding to
+%                   various events (e.g. simi triggers, tones, ect), where
+%                   N is the total number of blocks of the data (recording
+%                   sessions).
+% 
+% Outputs:
+% Events -          If given as an input, will append the new extracted
+%                   events into that input, otherwise, will just return a
+%                   new Event struct which only contains the SimiEvents
+%                   field.
 
 % check inputs and make defaults
 narginchk(3,4);
@@ -57,10 +79,10 @@ for iMarker=1:length(markerNames)
     
     if markerInd<=size(data,2)
         %get the frames where this marker was marked and save to events
-        Events(trialNum).SimiEvents.(markerNames{iMarker})=find(~isnan(data(:,markerInd)));
+        Events(blockNum).SimiEvents.(markerNames{iMarker})=find(~isnan(data(:,markerInd)));
     else
         %there was no data for this marker, just give empty struct
-        Events(trialNum).SimiEvents.(markerNames{iMarker})=[];
+        Events(blockNum).SimiEvents.(markerNames{iMarker})=[];
     end
 end
 
