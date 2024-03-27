7d0890ff357af9ec48ea4df35a952a705bdd5ce3
David Xing <davidxingg@gmail.com>
2024-03-27 13:22:12 -0500

    added behavior classification analysis

---
+++ MouseRecordings/behaviorClassification.m
@@ -0,0 +1,193 @@
+clear
+close all
+
+sessionDirs = {'Z:\David\ArenaRecordings\NeuropixelsTest\D020-062922-ArenaRecording',...
+    'Z:\David\ArenaRecordings\NeuropixelsTest\D024-111022-ArenaRecording',...
+    'X:\David\ArenaRecordings\D026-032923-ArenaRecording'};
+
+allBehvAlignPerms = [
+    1 2 3 4 5 6 7; ...
+    7 6 3 5 4 2 1; ...
+    1 2 4 5 3 6 7 ...
+    ];
+
+behvRegionLabels = {'Climb Up','Climb Down','Jump Down/Across','Walk Flat/Grid','Rearing/Still','Grooming','Eating'};
+
+for iSess = 1:length(sessionDirs)
+
+    behvAlignPerm = allBehvAlignPerms(iSess,:);
+    baseDir = sessionDirs{iSess};
+
+    %load in spiking data
+    % use 10ms bins
+    downsampleFactor = 10;
+    load(fullfile(baseDir,'ProcessedData','VideoSyncFrames.mat'))
+
+    if downsampleFactor == 10
+        load(fullfile(baseDir,'ProcessedData','NeuralFiringRates10msBins30msGauss.mat'),'allFRs','striatumInds','cortexInds');
+    end
+
+     % load UMAP projection
+    load(fullfile(baseDir,'ProcessedData','UMAP.mat'),'reduction','origDownsampEMGInd','gridXInds','gridYInds','regionAssignmentsFiltered','regionWatershedLabels')
+
+    %downsample the behavior labels to 10ms
+    %only keep 10ms bins where all 10 ms belong to a label
+    nDownSampBins = floor(length(regionAssignmentsFiltered)/downsampleFactor);
+    regionAssignmentsFilteredReshape = reshape(regionAssignmentsFiltered(1:nDownSampBins*downsampleFactor),downsampleFactor,nDownSampBins);
+    goodBins = find(all(regionAssignmentsFilteredReshape == mode(regionAssignmentsFilteredReshape)));
+    
+    origDownsampEMGIndBinned = origDownsampEMGInd(goodBins*10-5);
+    regionAssignmentsFilteredBinned = regionAssignmentsFiltered(goodBins*10-5);
+
+    %align neur data to reduction
+    currentDir = pwd;
+    cd(fullfile(baseDir,'ProcessedData'))
+    reducNeurInds = round(NeurEMGSync(origDownsampEMGIndBinned*20, frameEMGSamples, frameNeuropixelSamples, 'EMG')/300);
+    maxNeurSamples = round(frameNeuropixelSamples{1}{end}(end)/300)-1;
+    reducIndsToUse = find(reducNeurInds < maxNeurSamples);
+    cd(currentDir)
+
+    % get neural data corresponding to the UMAP time points
+    reducFRs = allFRs(:,reducNeurInds(reducIndsToUse));
+    clear allFRs
+
+    % don't use any points where there's nans in the FR data
+    nanReducPoints = any(isnan(reducFRs),1);
+    reducIndsToUse(nanReducPoints) = [];
+
+    reducFRs = reducFRs(:,~nanReducPoints);
+
+    %get the region classification for each point
+    behvLabels = regionAssignmentsFilteredBinned(reducIndsToUse);
+
+    % do control by shifting behavior labels
+    nShifts = 200;
+
+    for iShift = 1:nShifts+1
+        
+        tic
+
+        % first loop, just do actual classification without any shifting
+        if iShift ~= 1
+            minShiftAmount = 1000;
+            shiftAmount(iSess,iShift-1) = randperm(length(behvLabels)-2*minShiftAmount,1)+minShiftAmount;
+            behvLabels = circshift(behvLabels,shiftAmount(iSess,iShift-1));
+        end
+
+        %use 10-fold cross validation
+        cvBlocks = divideBlocks(1:length(behvLabels),10);
+
+%             for i = 1:10
+%                 cvBlocks{i} = i:10:length(behvLabels);
+%             end
+
+        goodNeuronsStr = find(mean(reducFRs(1:length(striatumInds),:),2)*100>0.2);
+        usedStrInds = 1:length(striatumInds);
+        usedStrInds = usedStrInds(goodNeuronsStr);
+
+        goodNeuronsCtx = find(mean(reducFRs(length(striatumInds)+1:end,:),2)*100>0.2);
+        usedCtxInds = length(striatumInds)+1:size(reducFRs,1);
+        usedCtxInds = usedCtxInds(goodNeuronsCtx);
+
+        for iBlock = 1:length(cvBlocks)
+
+            testInds = cvBlocks{iBlock};
+            trainInds = [cvBlocks{setdiff(1:length(cvBlocks),iBlock)}];
+
+            %         %do random forrest classification
+            %         tic
+            %         ctxRFClassifier = TreeBagger(10,reducFRs(length(striatumInds)+1:end,trainInds)',behvLabels(trainInds),...
+            %             Method="classification",...
+            %             OOBPrediction="on");
+            %         trainTime(iSess,iBlock) = toc;
+            %
+            %         strRFPredLabels{iBlock} = str2double(string(predict(strRFClassifier,reducFRs(1:length(striatumInds),testInds)')));
+            %         ctxRFPredLabels{iBlock} = str2double(string(predict(ctxRFClassifier,reducFRs(length(striatumInds)+1:end,testInds)')));
+            %
+            %         strRFBlockAccuracy(iSess,iBlock) = sum(strRFPredLabels{iBlock} == behvLabels(testInds)')/length(testInds);
+            %         ctxRFBlockAccuracy(iSess,iBlock) = sum(ctxRFPredLabels{iBlock} == behvLabels(testInds)')/length(testInds);
+
+
+                    % use kNN classifier
+            
+%                     features = reducFRs(usedStrInds,:);
+%                     strRFClassifier = fitcknn(features(:,trainInds)',behvLabels(trainInds),'NumNeighbors',100,'Standardize',1);
+%                     strRFPredLabels{iBlock} = str2double(string(predict(strRFClassifier,features(:,testInds)')));
+%                     strRFBlockAccuracy(iSess,iBlock) = sum(strRFPredLabels{iBlock} == behvLabels(testInds)')/length(testInds);
+% 
+%                     features = reducFRs(usedCtxInds,:);
+%                     ctxRFClassifier = fitcknn(features(:,trainInds)',behvLabels(trainInds),'NumNeighbors',100,'Standardize',1);
+%                     ctxRFPredLabels{iBlock} = str2double(string(predict(ctxRFClassifier,features(:,testInds)')));
+%                     ctxRFBlockAccuracy(iSess,iBlock) = sum(ctxRFPredLabels{iBlock} == behvLabels(testInds)')/length(testInds);
+
+
+            % use multi-class LDA
+            if iShift ==1
+
+                features = addHistory(reducFRs(usedStrInds,:),5,2,10);
+                strRFClassifier = fitcdiscr(features(:,trainInds)',behvLabels(trainInds));
+                strRFPredLabels{iBlock} = str2double(string(predict(strRFClassifier,features(:,testInds)')));
+                strRFBlockAccuracy(iSess,iBlock) = sum(strRFPredLabels{iBlock} == behvLabels(testInds)')/length(testInds);
+
+                features = addHistory(reducFRs(usedCtxInds,:),5,2,10);
+                ctxRFClassifier = fitcdiscr(features(:,trainInds)',behvLabels(trainInds));
+                ctxRFPredLabels{iBlock} = str2double(string(predict(ctxRFClassifier,features(:,testInds)')));
+                ctxRFBlockAccuracy(iSess,iBlock) = sum(ctxRFPredLabels{iBlock} == behvLabels(testInds)')/length(testInds);
+
+                testLabels{iBlock} = behvLabels(testInds);
+
+            else
+
+                features = addHistory(reducFRs(usedStrInds,:),5,2,10);
+                strRFClassifier = fitcdiscr(features(:,trainInds)',behvLabels(trainInds));
+                strRFPredLabelsShift{iBlock,iShift-1} = str2double(string(predict(strRFClassifier,features(:,testInds)')));
+                strRFBlockAccuracyShift(iSess,iBlock,iShift-1) = sum(strRFPredLabelsShift{iBlock,iShift-1} == behvLabels(testInds)')/length(testInds);
+
+                features = addHistory(reducFRs(usedCtxInds,:),5,2,10);
+                ctxRFClassifier = fitcdiscr(features(:,trainInds)',behvLabels(trainInds));
+                ctxRFPredLabelsShift{iBlock,iShift-1} = str2double(string(predict(ctxRFClassifier,features(:,testInds)')));
+                ctxRFBlockAccuracyShift(iSess,iBlock,iShift-1) = sum(ctxRFPredLabelsShift{iBlock,iShift-1} == behvLabels(testInds)')/length(testInds);
+
+                testLabelsShift{iBlock,iShift-1} = behvLabels(testInds);
+
+            end
+
+            %         %Also use SVM
+            %         %don't use low FR neurons
+            %         goodNeuronsStr = find(mean(reducFRs(1:length(striatumInds),:),2)*100>0.5);
+            %         goodNeuronsCtx = find(mean(reducFRs(length(striatumInds)+1:end,:),2)*100>0.5);
+            %
+            %         usedStrInds = 1:length(striatumInds);
+            %         usedStrInds = usedStrInds(goodNeuronsStr);
+            %         usedCtxInds = length(striatumInds)+1:size(reducFRs,1);
+            %         usedCtxInds = usedCtxInds(goodNeuronsCtx);
+            %
+            %         strNBClassifier = fitcecoc(reducFRs(usedStrInds,trainInds)',behvLabels(trainInds));
+            %         ctxNBClassifier = fitcecoc(reducFRs(usedCtxInds,trainInds)',behvLabels(trainInds));
+            %
+            %         strNBPredLabels{iBlock} = str2double(string(predict(strNBClassifier,reducFRs(usedStrInds,testInds)')));
+            %         ctxNBPredLabels{iBlock} = str2double(string(predict(ctxNBClassifier,reducFRs(usedCtxInds,testInds)')));
+            %
+            %         strNBBlockAccuracy(iBlock) = sum(strNBPredLabels{iBlock} == behvLabels(testInds)')/length(testInds);
+            %         ctxNBBlockAccuracy(iBlock) = sum(ctxNBPredLabels{iBlock} == behvLabels(testInds)')/length(testInds);
+            %
+
+        end
+
+        if iShift ==1
+            strAccuracy(iSess) = sum(cat(1,strRFPredLabels{:}) == cat(2,testLabels{:})') / length(cat(2,testLabels{:}));
+            ctxAccuracy(iSess) = sum(cat(1,ctxRFPredLabels{:}) == cat(2,testLabels{:})') / length(cat(2,testLabels{:}));
+        else
+            strAccuracyShift(iSess,iShift-1) = sum(cat(1,strRFPredLabelsShift{:,iShift-1}) == cat(2,testLabelsShift{:,iShift-1})') / length(cat(2,testLabelsShift{:,iShift-1}));
+            ctxAccuracyShift(iSess,iShift-1) = sum(cat(1,ctxRFPredLabelsShift{:,iShift-1}) == cat(2,testLabelsShift{:,iShift-1})') / length(cat(2,testLabelsShift{:,iShift-1}));
+        end
+
+        disp(toc)
+
+    end
+
+end
+
+
+
+% 
