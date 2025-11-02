04f44dc0a2704138687892b993dcc76fddb5199d
David Xing <davidxingg@gmail.com>
2025-11-02 11:03:35 -0600

    added run time logging, switched away from fisheye distortion

--- MouseRecordings/calcCameraDistortions.m
+++ MouseRecordings/calcCameraDistortions.m
@@ -1,12 +1,15 @@
 function distortionParams = calcCameraDistortions(baseDir,baseFilename,squareSize)
-%CALCCAMERADISTORTIONS Summary of this function goes here
-%   Detailed explanation goes here
+% distortionParams = calcCameraDistortions(baseDir,baseFilename,squareSize)
+% Uses matlab function estimateCameraParameters() to get the distortion
+% (not specifically fisheye distortion, but wide angle distortion)
+% parameters of the camera lenses based on a calibration session with
+% a checkerboard, with the size of the board squares indicated by squareSize
 
 nCameras = 4;
 
 for iCam = 1:nCameras
 
-    calVid = VideoReader(fullfile(baseDir,[baseFilename '_Camera' num2str(iCam) '-0000.avi']));
+    calVid = VideoReader(fullfile(baseDir,[baseFilename '_Camera' num2str(iCam-1) '-0000.avi']));
     
     % read frames
     allFrames = uint8([]);
@@ -15,19 +18,35 @@ for iCam = 1:nCameras
     % only use every 4 frames to save time and memory
     usedFrames = 1:4:vidNFrames;
 
+    tic
+    
     for iFrame = 1:length(usedFrames)
 
         frame = read(calVid,usedFrames(iFrame));
         allFrames(:,:,1,iFrame) = uint8(frame(:,:,1));
 
     end
+    
+    disp(['Loaded in video data from Camera ' num2str(iCam) ', time: ' num2str(toc)])
 
+    tic
+    
     [imagePoints,boardSize,usedFrames] = detectCheckerboardPoints(allFrames);%,'HighDistortion',false,'MinCornerMetric',0.05);
     worldPoints = generateCheckerboardPoints(boardSize,squareSize);
-
+    
+    disp(['Computed checkerboard points from Camera ' num2str(iCam) ', time: ' num2str(toc)])
+    
+    tic
+    
     imageSize = [size(allFrames,1) size(allFrames,2)];
-    distortionParams{iCam} = estimateFisheyeParameters(imagePoints,worldPoints,imageSize);
+    
+    %Using more points takes a lot more time (increases nonlinearly), so
+    %only use around 100 frames.
+    usedPoints = randperm(size(imagePoints,3),100);
+    distortionParams{iCam} = estimateCameraParameters(imagePoints(:,:,usedPoints),worldPoints,'ImageSize',imageSize);
 
+    disp(['Computed distortion parameters from Camera ' num2str(iCam) ', time: ' num2str(toc)])
+    
 end
 
 
